d75182aa38b2b31856188dd78a859a6fbfdbf9bd
PascalUlor <pascalulor@yahoo.com>
2019-11-26 15:53:29 +0100

    implement wise explorer and get current rrooms

--- src/room_details copy.py
+++
@@ -1,1 +0,0 @@
-[{"room_id": 1, "title": "Shop", "description": "You are standing in a small shop. A sign behind the mechanical shopkeeper says 'WILL PAY FOR TREASURE'.", "coordinates": "(59,60)", "elevation": 0, "terrain": "NORMAL", "players": [], "items": [], "exits": ["e"], "cooldown": 1.0, "errors": [], "messages": []}]
--- src/teleport.py
+++ src/teleport.py
@@ -40,17 +40,14 @@ def find_path(room_map, room_details, currentRoomID, destinationRoomName):
             if searched_room_id == destinationRoomName:
                 room_found = True
             if room_found:
-                correct_path = paths[searched_room_id]
-                directions = []
-                for i in range(len(correct_path) - 1):
-                    directions.append(directionToRoom(
-                        room_map, correct_path[i], correct_path[i + 1]))
+                actualPath = paths[searched_room_id]
+                directions = [directionToRoom(room_map, actualPath[i], actualPath[i + 1]) for i in range(len(actualPath) - 1)]
                 return directions
             q.enqueue(searched_room_id)
     return None
 
 
-def teleport(currentRoomID, destination_room_id_or_title_or_item, can_fly=False, can_dash=False):
+def teleport(currentRoomID, destination_room_id):
     room_details = []
     room_map = {}
     with open("room_details.py", "r") as f:
@@ -58,53 +55,16 @@ def teleport(currentRoomID, destination_room_id_or_title_or_item, can_fly=False,
     with open("room_graph.py", "r") as f:
         room_map = json.loads(f.read())
     # Traverse the map to find the path
-    path = find_path(room_map, room_details, currentRoomID, destination_room_id_or_title_or_item)
+    path = find_path(room_map, room_details, currentRoomID, destination_room_id)
+    print(f"FOLLOW THIS PATH {path} TO ROOM {destination_room_id}")
     # If path not found, print "path to {destination_room_id} not found"
     if not path:
-        print(f"path to {destination_room_id_or_title_or_item} not found")
+        print(f"path to {destination_room_id} not found")
         return
     # If path found, go through each room to the destination room
     for i in range(len(path)):
-        used_flight = False
-        used_dash = False
-        data = {}
-        # fly if can fly and terrain is elevated
-        if can_fly:
-            for room_info in room_details:
-                if str(room_info['room_id']) == currentRoomID:
-                    if 'terrain' in room_info and room_info['terrain'].lower() == "elevated":
-                        data = api_request('/fly', "POST", {
-                            "direction": path[i]})
-                        used_flight = True
-                        break
-                    else:
-                        break
-        # dash if can dash and didn't use fly and there is more rooms to dash through than 1
-        if can_dash and not used_flight:
-            directions_to_dash_through = [path[i]]
-            j = i + 1
-            while j < len(path):
-                if path[j] == directions_to_dash_through[j - 1]:
-                    directions_to_dash_through.append(path[j])
-                else:
-                    break
-            if len(directions_to_dash_through) > 1:
-                # get ids out of to_dash_through
-                ids_to_dash_through = []
-                cur = currentRoomID
-                for direction in directions_to_dash_through:
-                    next_room_id = room_map[cur][direction]
-                    ids_to_dash_through.append(next_room_id)
-                    cur = next_room_id
-                ids_to_dash_through = ','.join(ids_to_dash_through)
-                data = api_request('/dash', "POST", {
-                    "direction": path[i], "next_room_ids": ids_to_dash_through})
-                i += len(directions_to_dash_through) - 1
-                used_dash = True
-        # just walk if didn't use flight or dash
-        if not used_flight and not used_dash:
-            next_room_id = room_map[currentRoomID][path[i]]
-            data = api_request('/move', "POST", {
+        next_room_id = room_map[currentRoomID][path[i]]
+        data = api_request('/move', "POST", {
                 "direction": path[i], "next_room_id": next_room_id})
 
         print(data)
@@ -127,7 +87,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # add the filename argument to the parser
-    parser.add_argument("destination", help="The room you want to go to")
+    parser.add_argument("destination")
 
     # parse to get the argument
     args = parser.parse_args()
@@ -136,7 +96,7 @@ if __name__ == "__main__":
     current_room = get_current_room()
 
     # get the destination room
-    destination_room = args.destination
+    destination = args.destination
 
     # call teleport with the arg and current room
-    teleport(current_room, destination_room)
+    teleport(current_room, destination)
