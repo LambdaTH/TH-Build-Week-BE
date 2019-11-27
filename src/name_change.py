c964d78a45af3f15177e36fdea404c5cbcb3d3e4
PascalUlor <pascalulor@yahoo.com>
2019-11-27 10:17:29 +0100

    refactor name change

--- src/name_change.py
+++ src/name_change.py
@@ -2,35 +2,24 @@ import requests
 import time
 from decouple import config
 from teleport import teleport
+from api import api_request
 
-TOKEN = config("TOKEN")
 NEW_NAME = config("NAME")
-
-auth = {"Authorization": "Token " + TOKEN}
 
 
 def get_current_room():
-    res = requests.get(
-        "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/",
-        headers=auth
-    )
-    res_json = res.json()
-    time.sleep(res_json["cooldown"])
-    return res_json["room_id"]
+    res = api_request('/init', "GET")
+    return res["room_id"]
 
 
-def change_name():
-    res = requests.post(
-        "https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/",
-        headers=auth,
-        json={"name": NEW_NAME, "confirm": "aye"}
-    )
-    return res.json()
+def name_change():
+    res = api_request('/change_name', "POST", {"name": NEW_NAME, "confirm": "aye"})
+    return res
 
 
 if __name__ == "__main__":
     current_room = get_current_room()
-    change_name_room = "467"
-    teleport(str(current_room), change_name_room)
-    response = change_name()
+    name_change_room = "467"
+    teleport(str(current_room), name_change_room)
+    response = name_change()
     print(response)
